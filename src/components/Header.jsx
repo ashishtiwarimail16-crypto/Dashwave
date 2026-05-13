37ec4b01c7ae28d935ea8775a8e945489b2687dd
ashish <ashishtiwari21@gmail.com>
2026-05-13 11:33:32 +0530

    updated responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,15 +20,15 @@ function Header({
 
   return (
     <div
-      className={`border-b px-6 py-5 sticky top-0 z-40 backdrop-blur-xl ${
+      className={`border-b px-3 sm:px-4 py-2 text-sm sm:text-base sticky top-0 z-40 backdrop-blur-xl ${
         darkMode
           ? "bg-slate-900/80 border-white/10 text-white"
           : "bg-white border-gray-200 text-black"
       }`}
     >
-<div className="flex flex-col lg:flex-row lg:items-center justify-between gap-4">
+      <div className="flex flex-col lg:flex-row lg:items-center justify-between gap-4 w-full">
         {/* Left */}
-        <div className="flex items-center gap-4">
+        <div className="flex items-center justify-between w-full lg:w-auto gap-4">
 
           {!sidebar && (
             <button
@@ -43,14 +43,14 @@ function Header({
             </button>
           )}
 
-          <h1 className="text-2xl md:text-3xl font-bold">
+          <h1 className="text-xl sm:text-2xl md:text-3xl font-bold">
             DashWave
           </h1>
 
         </div>
 
         {/* Right */}
-      <div className="flex flex-wrap items-center gap-3">
+      <div className="flex flex-wrap sm:flex-nowrap items-center gap-2 sm:gap-3 w-full lg:w-auto overflow-x-auto">
 
           {/* Dark Mode */}
           <button
@@ -71,7 +71,7 @@ function Header({
           {/* Profile */}
           <button
             onClick={() => setShowProfile(true)}
-            className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-xl transition-all"
+            className="flex items-center gap-2  whitespace-nowrap bg-blue-500 hover:bg-blue-600 text-white px-3 sm:px-4 py-2 text-sm sm:text-base rounded-xl transition-all"
           >
             <User className="w-5 h-5" />
             Profile
@@ -80,7 +80,7 @@ function Header({
           {/* Sign Up */}
           <button
             onClick={() => setShowSignup(true)}
-            className="flex items-center gap-2 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-xl transition-all"
+            className="flex items-center gap-2 whitespace-nowrap bg-green-500 hover:bg-green-600 text-white px-3 sm:px-4 py-2 text-sm sm:text-base rounded-xl transition-all"
           >
             <UserPlus className="w-5 h-5" />
             Sign Up
@@ -89,7 +89,7 @@ function Header({
           {/* Login */}
           <button
             onClick={() => setShowLogin(true)}
-            className="flex items-center gap-2 bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded-xl transition-all"
+            className="flex items-center gap-2 whitespace-nowrap bg-purple-500 hover:bg-purple-600 text-white px-3 sm:px-4 py-2 text-sm sm:text-base rounded-xl transition-all"
           >
             <LogIn className="w-5 h-5" />
             Login
